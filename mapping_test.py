bc765704f5042ee1df28ad83a963ee8c5f182b13
Dinoxel <simaxel@live.com>
2022-06-06 23:22:09 +0200

    more testing

--- mapping_test.py
+++ mapping_test.py
@@ -1,40 +1,6 @@
 from game_classes import *
 
-# Chaque Biome doit avoir les propriétés de sa carte
-
 # Implémenter système de Fog of War avec zones accessibles ou non
-# Possibilité d'afficher une carte
-# Possibilité d'aller vers les 4 points cardinaux
-
-# # # # # # # # # # # #
-# # # # # # # #   # # # # # # # # # #
-# # # # #         #   |       # # x #
-# # #             e   # #           # # #
-# #                 # # # # # #         #
-#               # # # # # # # # # # # _ #
-#      [V]      #   x #             # D #
-#                     #             # # #
-# #       e         # #
-# #           # # # # #
-# # #       # # # # # #
-# # # # # # # # # # # #
-
-map1 = [
-    [1, 1, 1, 1, 1, 1, 1],
-    [1, 0, 0, 0, 0, 0, 1],
-    [1, 0, 3, 0, 0, 0, 1],
-    [1, 0, 0, 0, 0, 0, 1],
-    [1, 1, 1, 1, 1, 1, 1]
-]
-
-map_x = len(map1[0])
-map_y = len(map1)
-map_total = map_x * map_y
-# print(map_x, map_y, map_total)
-
-map2 = [
-    [1, 3, 0, 0, 1],
-]
 
 
 event = {
@@ -45,86 +11,104 @@ event = {
         "fix": False
     },
 }
-biome = {
-    "village": {
-        "name": "Village",
-        "x_loc": 1,
-        "y_loc": -2,
-        "fix": True
-    },
-}
+
+current_biome = Biome(**data_biomes["forest"])
+
+
+
+
 
 
 class Player_pos(Player):
-    def __init__(
-            self,
-    ):
+    def __init__(self):
         super().__init__()
-        self.x_pos = 0
-        self.y_pos = 0
-        self.pos = self.x_pos, self.y_pos
 
-        self.min_x_pos = -2
-        self.max_x_pos = 3
-        self.min_y_pos = -2
-        self.max_y_pos = 3
+        self.biome_area = current_biome.area
+        self.starting_pos = (5, 5)
+        self.y_pos, self.x_pos = self.starting_pos
 
-    def redefine_pos(self):
-        self.pos = self.x_pos, self.y_pos
+    def get_player_pos(self):
+        return self.y_pos, self.x_pos
+
+    def display_map(self, display_full_map=True):
+        replacing_dict = {
+            0: ".",  # Case
+            1: "▒",  # Mur
+            8: "C",  # Passage
+            7: "V",  # Village
+        }
+
+        if display_full_map:
+            area_to_display = self.biome_area
+        else:
+            def set_pos(map_area, pos):
+                return map_area[pos - 2:pos + 3]
+
+            area_to_display = [set_pos(row, self.x_pos) for row in set_pos(self.biome_area, self.y_pos)]
+
+        zone_list = list()
+
+        for row_num, row in enumerate(area_to_display):
+            col_list = list()
+            for col_num, col in enumerate(row):
+                if self.get_player_pos() == (row_num, col_num):
+                    col_list.append("P")
+                else:
+                    col_list.append(replacing_dict.get(col, "!"))
+            zone_list.append(' '.join(col_list))
+
+        if not display_full_map:
+            zone_list[2] = zone_list[2][:4] + 'P' + zone_list[2][5:]
+
+        print("╔" + "═" * (len(zone_list[0])) + '╗')
+        for line in zone_list:
+            print("║" + line + "║")
+        print("╚" + "═" * (len(zone_list[0])) + '╝')
 
     def display_pos(self):
-        print(self.pos)
+        self.display_map()
+        print(self.get_player_pos())
+
+    def event_checker(self):
+        event_dict = {
+            (5, 6): "You found a chest!"}
+
+        for k, v in event_dict.items():
+            if k == (self.y_pos, self.x_pos):
+                print(v)
 
     def go(self, where):
-        if where == "right":
-            if self.x_pos != self.max_x_pos:
-                self.x_pos += 1
-                print(f"Moved {where}")
-            else:
-                print(f"Cannot go {where} anymore")
+        previous_pos = self.x_pos, self.y_pos
 
-        elif where == "left":
-            if self.x_pos != self.min_x_pos:
-                self.x_pos -= 1
-                print(f"Moved {where}")
-            else:
-                print(f"Cannot go {where} anymore")
+        pos_dict = {
+            "right": {"row": 0, "col": 1},
+            "left": {"row": 0, "col": -1},
+            "up": {"row": -1, "col": 0},
+            "down": {"row": 1, "col": 0}
+        }
 
-        elif where == "up":
-            if self.y_pos != self.max_y_pos:
-                self.y_pos += 1
-                print(f"Moved {where}")
-            else:
-                print(f"Cannot go {where} anymore")
+        for direction, travel in pos_dict.items():
+            if direction == where and self.biome_area[self.y_pos + travel["row"]][self.x_pos + travel["col"]] != 1:
+                if travel["row"] == 0:
+                    self.x_pos = self.x_pos + travel["col"]
+                else:
+                    self.y_pos = self.y_pos + travel["row"]
 
-        elif where == "down":
-            if self.y_pos != self.min_y_pos:
-                self.y_pos -= 1
-                print(f"Moved {where}")
-            else:
-                print(f"Cannot go {where} anymore")
-
-        self.redefine_pos()
-
-forest_biome = Biome(**(data_biomes["forest"]))
+        if previous_pos != (self.x_pos, self.y_pos):
+            print(f"Moved {where}")
+            self.display_map()
+            self.event_checker()
+        else:
+            print(f"Cannot go to the {where}")
 
 
-print(vars(forest_biome))
 
 
-class Biome_map(Biome):
-    def __init__(
-            self,
-    ):
-        super().__init__("Jungle", 1, ())
-        self.area = [
-            [1, 3, 0, 0, 1],
-        ]
-        self.start_x_pos = 0
-        self.start_y_pos = 1
+# forest_biome = Biome(**data_biomes["forest"])
+# print(vars(forest_biome))
 
 
-map_test = Biome_map()
+
 
 
 player = Player_pos()
@@ -132,13 +116,13 @@ player = Player_pos()
 while True:
     player_input = input("Where do you want to go?\n> ")
 
-    if player_input == "left":
+    if player_input in ["left", "l"]:
         player.go("left")
-    elif player_input == "right":
+    elif player_input in ["right", "r"]:
         player.go("right")
-    elif player_input == "up":
+    elif player_input in ["up", "u", "top", "t"]:
         player.go("up")
-    elif player_input == "down":
+    elif player_input in ["down", "bot", "b", "d"]:
         player.go("down")
     elif player_input == "pos":
         player.display_pos()
